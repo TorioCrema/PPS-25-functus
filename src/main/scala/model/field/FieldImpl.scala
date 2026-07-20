2dbad1f87c2ff019267eca4580dbe8f60ca3d370
Luca Ferar Tofan Nicolae <47209041+LuconeVevo@users.noreply.github.com>
2026-07-20 10:48:39 +0200

    feat: field (#12)
    * test: added tests for field
    
    * feat: added implementations for field
    
    * test: added new tests for replace and card order
    
    * refactor: refactor of duplicated code in field implementation
    
    * fix: fixed bug in getcard function, now extraction works with n cards in the field

---
+++ src/main/scala/model/field/FieldImpl.scala
@@ -0,0 +1,22 @@
+package org.pps.functus
+package model.field
+
+import model.deck.card.Card
+
+case class FieldImpl(private val cards: Vector[Card] = Vector.empty) extends Field:
+
+  override def length: Int = cards.length
+
+  override def replace(index: Int, card: Card): (Card, Field) =
+    checkIndex(index)
+    (cards(index), copy(cards.updated(index, card)))
+
+  override def addCard(card: Card): Field = copy(cards :+ card)
+
+  override def getCard(index: Int): (Card, Field) =
+    checkIndex(index)
+    (cards(index), copy(cards.patch(index, Nil, 1)))
+
+  private def checkIndex(index: Int): Unit =
+    if index < 0 || index >= cards.length then
+      throw IndexOutOfBoundsException(s"Index $index is out of bounds for field of length ${cards.length}")
---
+++ src/test/scala/model/FieldTest.scala
@@ -0,0 +1,56 @@
+package org.pps.functus
+package model
+
+import model.deck.card.{Card, CardImpl}
+import model.deck.card.Suit.Swords
+import model.field.{Field, FieldImpl}
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FieldTest extends AnyFlatSpec with Matchers:
+
+  def card = CardImpl(0, Swords)
+  def first = CardImpl(0, Swords)
+  def second = CardImpl(1, Swords)
+  val fieldTest: Field = FieldImpl()
+
+  "A new Field" should "have 0 cards" in:
+    val field: Field = FieldImpl()
+    field.length should be(0)
+
+  it should "throw an IndexOutOfBoundsException when replacing a card in an empty field" in:
+    an[IndexOutOfBoundsException] should be thrownBy
+      FieldImpl().replace(0, card)
+
+  it should "throw an IndexOutOfBoundsException when getting a card from an empty field" in:
+    an[IndexOutOfBoundsException] should be thrownBy
+      FieldImpl().getCard(0)
+
+  it should "contain one card after adding a card" in:
+    val field = FieldImpl().addCard(card)
+    field.length should be(1)
+    field.getCard(0)._1 should be(card)
+
+  it should "maintain card order when adding multiple cards" in:
+    val field = FieldImpl().addCard(first).addCard(second)
+    field.length should be(2)
+    field.getCard(0)._1 should be(first)
+    field.getCard(1)._1 should be(second)
+
+  it should "replace a card correctly and return the replaced card" in:
+    val field = FieldImpl().addCard(first)
+    val result: (Card, Field) = field.replace(0, second)
+    result._1 should be(first)
+    result._2.length should be(1)
+    result._2.getCard(0)._1 should be(second)
+
+  it should "throw an IndexOutOfBoundsException when replacing out of range" in:
+    val field = FieldImpl().addCard(card)
+    an[IndexOutOfBoundsException] should be thrownBy
+      field.replace(5, card)
+
+  it should "throw an IndexOutOfBoundsException when getting a card out of range" in:
+    val field = FieldImpl().addCard(card)
+    an[IndexOutOfBoundsException] should be thrownBy
+      field.getCard(5)
