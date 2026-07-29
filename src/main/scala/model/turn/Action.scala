d367c7d628d0eea9f1830d2e723ee48cfd51ef7c
Simone Zama <simone.zama@studio.unibo.it>
2026-07-29 11:35:59 +0200

    Feature: Turn (#28)
    * feat: Board interface
    
    * feat: Action
    
    * feat: added a method for drawing a card from a player field
    
    * feat: added a method for creating custom boards for test purposes
    
    * feat: Observe action execution
    
    * feat: added nextPlayer method to know whos turn is it
    
    * test: added tests for switching turn logic
    
    * feat: now player has a method to take the next player
    
    * feat: added a list for cards in the field
    
    * feat: added method to add a card without replacing other cards inside the field of a player from a board
    
    * fix: fixed append problem with placeCardInField
    
    * fix: fixed boardtest indexOutOfBound
    
    * test: added tests for BoardFactory
    
    * feat: FirstTurn and SimpleTurn factories
    
    * feat: Draw, Activate & ChooseReplace on Turn
    
    * fix: now kingTopDiscardStack returns the King on top of the discard pile and the updated Board with the King removed from the discard pile. And updated tests
    
    * feat: DrawKing & ChooseDiscard actions, refactor
    
    * fix: remove unused imports
    
    * ref: remove empty source file

--- src/main/scala/model/turn/Action.scala
+++ src/main/scala/model/turn/Action.scala
@@ -2,22 +2,39 @@ package org.pps.functus
 package model.turn
 
 enum Action:
+  /** Action used to observe cards on the player's field.
+    */
   case Observe
+
+  /** Action that is used to restore the board after [[Observe]].
+    */
   case Confirm
+
+  /** Ends the turn. */
   case EndTurn
+
+  /** Draws from the deck to the player's hand. */
   case Draw
+
+  /** Activates the card in the player's hand. */
   case Activate
+
+  /** Calls Cactus */
   case Cactus
+
+  /** Draws the king from the top of the discard pile. */
   case DrawKing
+
+  /** Picks a card to discard on top of the discard pile. */
   case ChooseDiscard(index: Int)
+
+  /** Picks a card to replace with the card in the player's hand. */
   case ChooseReplace(index: Int)
-  case Discard
 
   def next: List[Action] = this match
     case Observe          => List(Confirm)
     case Confirm | Cactus => List(EndTurn)
     case Draw | DrawKing  => List(Activate)
     case ChooseReplace(_) => List(Cactus, EndTurn)
-    case ChooseDiscard(_) => List(Discard)
-    case Discard          => List(Draw)
+    case ChooseDiscard(_) => List(Draw)
     case _                => Nil
--- src/main/scala/model/turn/Turn.scala
+++ src/main/scala/model/turn/Turn.scala
@@ -4,22 +4,22 @@ package model.turn
 import model.deck.card.Card
 import model.board.{Board, Player}
 import model.turn.Action.*
-import model.deck.sugar.FieldDSL.*
-import model.deck.sugar.FieldDSL.given
 import model.deck.sugar.CardDSL.*
 
-case class Turn(hand: List[Card], board: Board, player: Player, actions: List[Action]):
-  def act(action: Action): Turn = action match
-    case Observe =>
-      List(0, 0).foldLeft(this)((turn, index) => turn.drawnFromField(index)).withActions(Observe.next)
-    case Confirm =>
-      val newBoard = hand.foldLeft(board)((b, card) => b.placeCardInField(card, player, Option.empty))
-      Turn(Nil, newBoard, player, Confirm.next)
-    case Draw =>
-      val (drawn, newBoard) = board.draw
-      Turn(List(drawn), newBoard, player, Draw.next)
-    case Activate => Turn(hand, board, player, (0 until board.getField(player).length).map(ChooseReplace(_)).toList)
-    case ChooseReplace(index) => Turn(Nil, board.replace(player, index, hand.head), player, ChooseReplace(index).next)
+/** Turn class that allows to play a turn from start to finish via the [[act]] method.
+  * @param hand
+  *   the player's hand
+  * @param board
+  *   the current game board
+  * @param player
+  *   the player playing this turn
+  * @param actions
+  *   the available actions
+  * @param cactus
+  *   whether cactus was called
+  */
+case class Turn(hand: List[Card], board: Board, player: Player, actions: List[Action], cactus: Boolean = false):
+  private val observableCards = 2
 
   private def drawnFromField(index: Int) =
     val (drawn, newBoard) = board.drawPlayerCard(player, index)
@@ -27,11 +27,66 @@ case class Turn(hand: List[Card], board: Board, player: Player, actions: List[Ac
 
   private def withActions(newActions: List[Action]): Turn = Turn(hand, board, player, newActions)
 
+  /** Executes the given [[Action]] and returns the next phase of the turn
+    * @param action
+    *   the [[Action]] to execute
+    * @return
+    *   the next phase of the [[Turn]]
+    */
+  def act(action: Action): Turn = action match
+    case Observe =>
+      List
+        .fill(observableCards)(0)
+        .foldLeft(this)((turn, index) => turn.drawnFromField(index))
+        .withActions(action.next)
+    case Confirm =>
+      val newBoard = hand.foldLeft(board)((b, card) => b.placeCardInField(card, player, Option.empty))
+      Turn(Nil, newBoard, player, action.next)
+    case Draw =>
+      val (drawn, newBoard) = board.draw
+      Turn(drawn :: hand, newBoard, player, action.next)
+    case DrawKing =>
+      val (drawnKing, newBoard) = board.kingTopDiscardStack()
+      Turn(drawnKing :: Nil, newBoard, player, action.next)
+    case Activate => Turn(hand, board, player, (0 until board.getField(player).length).map(ChooseReplace(_)).toList)
+    case ChooseReplace(index) =>
+      Turn(Nil, board.replace(player, index, hand.head), player, action.next)
+    case ChooseDiscard(index) =>
+      val (chosen, _) = board.getField(player).getCard(index)
+      if chosen.value == board.getTopDiscardStack.value then
+        Turn(Nil, board.discard(chosen).drawPlayerCard(player, index)._2, player, action.next)
+      else
+        val (drawn, boardAfterDraw) = board.draw
+        val newBoard = boardAfterDraw.placeCardInField(drawn, player, Option.empty)
+        Turn(Nil, newBoard, player, action.next)
+    case EndTurn => Turn(hand, board, player, EndTurn.next, cactus)
+    case Cactus  => Turn(hand, board, player, Cactus.next, true)
+
+  /** @return
+    *   [[true]] if the [[Turn]] is over.
+    */
+  def isOver: Boolean = this match
+    case Turn(_, _, _, Nil, _) => true
+    case _                     => false
+
+/** Factory methods for the [[Turn]] class
+  */
 object Turns:
+
   object FirstTurn:
+    /** Creates a [[Turn]] that allows the [[Player]] to observe cards on his field.
+      */
     def apply(board: Board, player: Player): Turn = Turn(Nil, board, player, List(Observe))
 
   object SimpleTurn:
+    /** Creates a [[Turn]]
+      * @param board
+      *   the [[Board]] at the start of the [[Turn]]
+      * @param player
+      *   the [[Player]] that plays the [[Turn]]
+      * @return
+      *   the [[Turn]]
+      */
     def apply(board: Board, player: Player): Turn = board.discardPile.length match
       case 0 => Turn(Nil, board, player, List(Draw))
       case _ =>
@@ -39,5 +94,5 @@ object Turns:
           case `king` => Turn(Nil, board, player, List(Draw, DrawKing))
           case _      =>
             val nextActions =
-              List(Draw) ++ (0 until board.getField(player).length).map(index => ChooseDiscard(index))
+              Draw :: (0 until board.getField(player).length).map(index => ChooseDiscard(index)).toList
             Turn(Nil, board, player, nextActions)
