5147c907d9b607d9a9cddf5ed4c1c8a3cf9a62a6
Simone Zama <simone.zama@studio.unibo.it>
2026-07-29 08:48:32 +0200

    feat: DrawKing & ChooseDiscard actions, refactor

--- src/test/scala/model/ActionTest.scala
+++ src/test/scala/model/ActionTest.scala
@@ -7,28 +7,25 @@ import org.scalatest.matchers.should.Matchers
 
 class ActionTest extends AnyFlatSpec with Matchers:
   "Observe action" should "have Confirm as next action" in:
-    Observe.next should be(List(Confirm))
+    Observe.next should be(Confirm :: Nil)
 
   "Confirm action" should "have EndTurn as next action" in:
-    Confirm.next should be(List(EndTurn))
+    Confirm.next should be(EndTurn :: Nil)
 
   "Draw action" should "have Activate as next actions" in:
-    Draw.next should be(List(Activate))
+    Draw.next should be(Activate :: Nil)
 
   "Cactus action" should "have EndTurn as next action" in:
-    Cactus.next should be(List(EndTurn))
+    Cactus.next should be(EndTurn :: Nil)
 
   "EndTurn action" should "have no next actions" in:
     EndTurn.next should be(Nil)
 
   "DrawKing" should "have Activate as next action" in:
-    DrawKing.next should be(List(Activate))
+    DrawKing.next should be(Activate :: Nil)
 
-  "Discard" should "have Draw as next action" in:
-    Discard.next should be(List(Draw))
-
-  "ChooseDiscard" should "have Discard has next action" in:
-    ChooseDiscard(0).next should be(List(Discard))
+  "ChooseDiscard" should "have Draw has next action" in:
+    ChooseDiscard(0).next should be(Draw :: Nil)
 
   "ChooseReplace" should "have Cactus and EndTurn as next actions" in:
-    ChooseReplace(0).next should be(List(Cactus, EndTurn))
+    ChooseReplace(0).next should be(Cactus :: EndTurn :: Nil)
--- src/test/scala/model/SimpleTurnTest.scala
+++ src/test/scala/model/SimpleTurnTest.scala
@@ -1,10 +1,9 @@
 package org.pps.functus
 package model
 
-import model.board.BoardFactory.CustomBoard
 import model.deck.DeckImpl
 import model.deck.sugar.CardDSL.*
-import model.deck.sugar.FieldDSL.{*, given}
+import model.deck.sugar.FieldDSL.given
 import model.deck.card.Suit.*
 import model.turn.Action.{ChooseDiscard, ChooseReplace}
 import model.turn.Turns.SimpleTurn
@@ -22,40 +21,82 @@ class SimpleTurnTest extends AnyFlatSpec with Matchers:
   private val aceOfPentacles = ace of Pentacles
   private val player1Field = threeOfCups and twoOfSwords
   private val player2Field = fiveOfWands and aceOfPentacles
-  private val board = BoardImpl(players = Map((Player1, player1Field), (Player2, player2Field)))
+  private val startingBoard =
+    BoardImpl(deck = DeckImpl(Vector(two of Wands)), players = Map((Player1, player1Field), (Player2, player2Field)))
+  private val player = Player1
+  private val simpleTurn = SimpleTurn(startingBoard, player)
+  private val boardWithThreeInDiscard = startingBoard.discard(three of Pentacles)
+  private val discardableTurn = SimpleTurn(boardWithThreeInDiscard, player)
 
   "SimpleTurn" should "have draw as next action if top of discard stack is empty" in:
-    val startingBoard = CustomBoard(List(player1Field, player2Field))
-    SimpleTurn(startingBoard, Player1).actions should be(List(Draw))
+    simpleTurn.actions should be(Draw :: Nil)
 
   it should "have draw and draw king as next actions if top of discard stack is a king" in:
-    val startingBoard = BoardImpl(discardPile = List(king of Swords))
-    SimpleTurn(startingBoard, Player1).actions should be(List(Draw, DrawKing))
+    SimpleTurn(startingBoard.discard(king of Swords), player).actions should be(Draw :: DrawKing :: Nil)
 
-  it should "have draw and choose discard as next actions discard stack isn't empty and top isn't a king" in:
-    val startingBoard =
-      BoardImpl(discardPile = List(five of Wands), players = Map((Player1, player1Field), (Player2, player2Field)))
-    val expectedActions = List(Draw) ++ (0 until player1Field.length).toList.map(index => ChooseDiscard(index))
-    SimpleTurn(startingBoard, Player1).actions should be(expectedActions)
+  it should "have draw and choose discard as next actions if discard stack isn't empty and top isn't a king" in:
+    val expectedActions = Draw :: (0 until player1Field.length).toList.map(index => ChooseDiscard(index))
+    SimpleTurn(startingBoard.discard(five of Wands), player).actions should be(expectedActions)
 
   it should "draw to hand when executing Draw" in:
-    val startingBoard =
-      BoardImpl(deck = DeckImpl(Vector(two of Wands)), players = Map((Player1, player1Field), (Player2, player2Field)))
-    val afterDraw = SimpleTurn(startingBoard, Player1).act(Draw)
+    val afterDraw = simpleTurn.act(Draw)
     afterDraw.hand.length should be(1)
     afterDraw.hand.head should be(two of Wands)
 
+  it should "draw from discard pile when drawing king" in:
+    val boardWithKingInDiscard = startingBoard.discard(king of Swords)
+    val afterDrawKing = SimpleTurn(boardWithKingInDiscard, player).act(DrawKing)
+    afterDrawKing.actions should be(Activate :: Nil)
+    afterDrawKing.board.discardPile.isEmpty should be(true)
+    afterDrawKing.hand should be((king of Swords) :: Nil)
+
+  it should "throw IllegalStateException when drawing without a king on the discard pile" in:
+    an[IllegalStateException] should be thrownBy SimpleTurn(startingBoard.discard(threeOfCups), player).act(DrawKing)
+
   it should "have choose replace as next actions" in:
-    val startingBoard =
-      BoardImpl(deck = DeckImpl(Vector(two of Wands)), players = Map((Player1, player1Field), (Player2, player2Field)))
-    val afterDraw = SimpleTurn(startingBoard, Player1).act(Draw)
+    val afterDraw = simpleTurn.act(Draw)
     afterDraw.act(Activate).actions should be((0 until player1Field.length).map(ChooseReplace(_)))
 
-  it should "replace after picking index" in:
-    val startingBoard =
-      BoardImpl(deck = DeckImpl(Vector(two of Wands)), players = Map((Player1, player1Field), (Player2, player2Field)))
-    val replacedField = (two of Wands) and twoOfSwords
-    val expectedBoard =
-      BoardImpl(DeckImpl(Vector()), List(threeOfCups), Map((Player1, replacedField), (Player2, player2Field)))
-    val afterReplace = SimpleTurn(startingBoard, Player1).act(Draw).act(ChooseReplace(0))
-    afterReplace.board should be(expectedBoard)
+  it should "replace card in player field and discard after picking index" in:
+    val afterDraw = simpleTurn.act(Draw)
+    for index <- 0 until player1Field.length
+    do
+      val replacedCard = afterDraw.board.getField(player).getCard(index)._1
+      val afterReplace = afterDraw.act(ChooseReplace(index))
+      afterReplace.hand.isEmpty should be(true)
+      afterReplace.board.discardPile.head should be(replacedCard)
+      val replacedField = afterReplace.board.getField(player)
+      replacedField.getCard(index)._1 should be(two of Wands)
+      replacedField.length should be(player1Field.length)
+
+  it should "have cactus and end turn as next actions after replacing" in:
+    val afterReplace = simpleTurn.act(Draw).act(ChooseReplace(0))
+    afterReplace.actions should be(Cactus :: EndTurn :: Nil)
+
+  it should "end with cactus" in:
+    val afterCactus = simpleTurn.act(Draw).act(ChooseReplace(0)).act(Cactus)
+    afterCactus.cactus should be(true)
+    afterCactus.actions should be(EndTurn :: Nil)
+
+  it should "be over after end turn action" in:
+    val afterEndTurn = simpleTurn.act(Draw).act(ChooseReplace(0)).act(EndTurn)
+    afterEndTurn.isOver should be(true)
+
+  it should "discard without penalty when discarding the correct value" in:
+    val afterCorrectDiscard = discardableTurn.act(ChooseDiscard(0))
+    afterCorrectDiscard.board.getField(player).length should be(player1Field.length - 1)
+    afterCorrectDiscard.board.discardPile.length should be(2)
+    afterCorrectDiscard.board.discardPile.head should be(threeOfCups)
+
+  it should "apply penalty when discarding the wrong value" in:
+    val afterWrongDiscard = discardableTurn.act(ChooseDiscard(1))
+    afterWrongDiscard.board.getField(player).length should be(player1Field.length + 1)
+    afterWrongDiscard.board.getTopDiscardStack should be(three of Pentacles)
+
+  it should "have draw as next actions after choosing discard" in:
+    for discardIndex <- 0 until player1Field.length
+    do SimpleTurn(boardWithThreeInDiscard, player).act(ChooseDiscard(discardIndex)).actions should be(Draw :: Nil)
+
+  it should "throw an IndexOutOfBounds exception when discarding beyond the player's field length" in:
+    an[IndexOutOfBoundsException] should be thrownBy discardableTurn.act(ChooseDiscard(player1Field.length))
+    an[IndexOutOfBoundsException] should be thrownBy discardableTurn.act(ChooseDiscard(-1))
