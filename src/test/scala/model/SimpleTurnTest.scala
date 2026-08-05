b37b1275ad373de87d44d83fe836bc63c8ef068c
Simone Zama <simone.zama@studio.unibo.it>
2026-08-05 11:18:38 +0200

    feat: card effects (#44)
    * feat: card effects
    
    * ref: Turn & tests

--- src/test/scala/model/SimpleTurnTest.scala
+++ src/test/scala/model/SimpleTurnTest.scala
@@ -82,14 +82,16 @@ class SimpleTurnTest extends AnyFlatSpec with Matchers:
     val afterEndTurn = simpleTurn.act(Draw).act(Activate).act(ChooseReplace(0)).act(EndTurn)
     afterEndTurn.isOver should be(true)
 
-  it should "draw the chosen card to discard to the player's hand" in:
-    val afterChooseDiscard = discardableTurn.act(ChooseDiscard(0))
-    afterChooseDiscard.hand should be(discardableTurn.board.getField(player).getCard(0)._1 :: Nil)
+  it should "draw the chosen card to the player's hand" in:
+    for i <- 0 until player1Field.length
+    do
+      val afterChooseDiscard = discardableTurn.act(ChooseDiscard(i))
+      afterChooseDiscard.hand should be(discardableTurn.board.getField(player).getCard(i)._1 :: Nil)
 
   it should "discard without penalty when discarding the correct value" in:
     val afterCorrectDiscard = discardableTurn.act(ChooseDiscard(0)).act(Discard(0))
     afterCorrectDiscard.board.getField(player).length should be(player1Field.length - 1)
-    afterCorrectDiscard.board.discardPile.length should be(2)
+    afterCorrectDiscard.board.discardPile.length should be(discardableTurn.board.discardPile.length + 1)
     afterCorrectDiscard.board.discardPile.head should be(threeOfCups)
 
   it should "apply penalty when discarding the wrong value" in:
