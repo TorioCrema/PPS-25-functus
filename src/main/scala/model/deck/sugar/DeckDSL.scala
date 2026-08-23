84bdafde5a9118e5863b98b21296805c51de048a
LucaFerar <luca.ferartofan@studio.unibo.it>
2026-08-23 17:25:44 +0200

    feat: added possibility to create a deck with just one card for testing and added conversion from cardbuilder to deck with new documentation

--- src/main/scala/model/deck/sugar/DeckDSL.scala
+++ src/main/scala/model/deck/sugar/DeckDSL.scala
@@ -8,10 +8,16 @@ import model.deck.card.Card
   *
   * Usage:
   * {{{
-  *   import model.deck.DeckDsl.*
+  *   import model.deck.sugar.DeckDSL.*
   *
-  *   val d1 = deck standard
-  *   val d2 = deck con { Asso di Cuori | Re di Picche }
+  *   val d1 = deck()
+  *   val d2 = deck from (ace of Cups | two of Swords)
+  *   val d3 = deck fromShuffled (ace of Cups | two of Swords)
+  *   val d4 = deck single (ace of Cups)
+  *   val d5 = deck from List(ace of Cups, two of Swords)
+  *
+  *   // CardBuilder converts automatically to Deck via given Conversion
+  *   val d6: Deck = (ace of Cups) | (two of Swords)
   * }}}
   */
 object DeckDSL:
@@ -21,8 +27,43 @@ object DeckDSL:
     /** Create a standard Italian Deck of cards. */
     infix def apply(): Deck = DeckFactory()
 
-    /** Create a deck using [[CardBuilder]]. */
+    /** Creates a [[Deck]] from a [[CardBuilder]] chain.
+      *
+      * @param builder
+      *   the [[CardBuilder]] containing the cards
+      * @return
+      *   a [[Deck]] with the cards in the builder, in insertion order
+      */
     infix def from(builder: CardBuilder): Deck = DeckImpl(builder.cards)
+
+    /** Creates a [[Deck]] from a [[List]] of [[Card]]s.
+      *
+      * @param cards
+      *   the list of cards
+      * @return
+      *   a [[Deck]] with the given cards, in list order
+      */
+    infix def from(cards: List[Card]): Deck = DeckImpl(cards.toVector)
+
+    /** Creates a shuffled [[Deck]] from a [[CardBuilder]] chain.
+      *
+      * @param builder
+      *   the [[CardBuilder]] containing the cards
+      * @return
+      *   a shuffled [[Deck]] with the cards in the builder
+      */
+    infix def fromShuffled(builder: CardBuilder): Deck = DeckImpl(builder.cards).shuffle()
+
+    /** Creates a [[CardBuilder]] containing a single [[Card]].
+      *
+      * Useful when a one-card deck or a one-card builder chain is needed.
+      *
+      * @param card
+      *   the card to wrap
+      * @return
+      *   a [[CardBuilder]] with the single card
+      */
+    def single(card: Card): CardBuilder = CardBuilder(Vector(card))
 
     /** Accumulates cards via the `|` operator.
       *
@@ -35,6 +76,10 @@ object DeckDSL:
     case class CardBuilder(cards: Vector[Card]):
       /** Appends a card to this builder. */
       infix def |(other: Card): CardBuilder = CardBuilder(cards :+ other)
+
+    /** Allows a [[CardBuilder]] to be used wherever a [[Deck]] is expected.
+      */
+    given Conversion[CardBuilder, Deck] = b => DeckImpl(b.cards)
 
     /** Allows any [[Card]] to start a [[CardBuilder]] chain using `|`. */
     extension (card: Card)
